082e56c6b8a8145dc185902339a7cd819262f24c
jmaksimik <jmaks@me.com>
2022-12-26 22:47:50 -0500

    Add CRUD functionality to dogs; add feeding model and attach to dog details; add CRUD operations for feeding

--- main_app/models.py
+++ main_app/models.py
@@ -9,4 +9,20 @@ class Dog(models.Model):
     age = models.IntegerField()
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'dog_id: self:id'})
+        return reverse('detail', kwargs={'dog_id': self.id})
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0])
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView): 
+    model = Dog
+    fields = ['breed', 'description', 'age'] 
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id = dog_id)
 
 def home(request):
     return render(request, 'home.html')
@@ -17,4 +38,5 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dogs/detail.html', {'dog': dog})
+    feeding_form = FeedingForm()
+    return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form})
